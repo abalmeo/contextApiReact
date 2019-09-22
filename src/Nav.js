ef460317c64b0e998159f0d4c27db80b686f29f8
Alvin Balmeo <albalmeo@gmail.com>
2019-09-22 15:27:16 -0700

    Setting up components

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap';
+
+const Nav = () => {
+  return (
+<>
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/">
+      Alvin
+    </Navbar.Brand>
+  </Navbar>
+</>
+  )
+}
+
+Nav.propTypes = {
+
+}
+
+export default Nav
