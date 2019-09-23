305696891478fd5e6d069e01abe081e9e65c2cea
Alvin Balmeo <albalmeo@gmail.com>
2019-09-22 23:27:19 -0700

    Building app

---
+++ build/precache-manifest.f954df2408260352141399ca06b363a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2c8d443042867a3fac824d8e029bd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2eb8df02cc44652c8a85",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "52aa13818f1d71cd2240",
+    "url": "/static/js/2.b098c3c3.chunk.js"
+  },
+  {
+    "revision": "2eb8df02cc44652c8a85",
+    "url": "/static/js/main.851bf372.chunk.js"
+  },
+  {
+    "revision": "55cee691157bc4a747fd",
+    "url": "/static/js/runtime-main.482f7e1a.js"
+  }
+]);
